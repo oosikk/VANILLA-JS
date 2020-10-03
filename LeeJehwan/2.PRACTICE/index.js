4b628303121701342d8466b94b14a1d5c7c83151
leejehwan <wpghks7@naver.com>
2020-10-04 02:20:51 +0900

    2.2 JS DOM Functions

--- LeeJehwan/2.PRACTICE/index.js
+++ LeeJehwan/2.PRACTICE/index.js
@@ -1,35 +1,6 @@
-function sayHello(name, age) {
-  return `hello ${name} you are ${age} years old`;
-}
+const title = document.getElementById("title");
 
-const greetNicolas = sayHello("Nico", 14);
-console.log(greetNicolas);
+console.log(title);
+console.error("err");
 
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  multiply: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  square: function (a, b) {
-    return a ** b;
-  },
-};
-
-const plus = calculator.plus(5, 5);
-console.log(plus);
-const minus = calculator.minus(5, 5);
-console.log(minus);
-const multiply = calculator.multiply(5, 5);
-console.log(multiply);
-const divide = calculator.divide(5, 5);
-console.log(divide);
-const square = calculator.square(5, 5);
-console.log(square);
+title.innerHTML = "Hi! From JS";
