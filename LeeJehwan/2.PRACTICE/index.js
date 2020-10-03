c6ead5c7eae5108bf5cfaa32b693f4bdf0af19ff
leejehwan <wpghks7@naver.com>
2020-10-04 02:31:25 +0900

    2.5 If, else, and, or

--- LeeJehwan/2.PRACTICE/index.js
+++ LeeJehwan/2.PRACTICE/index.js
@@ -11,3 +11,17 @@ function handleClick() {
   title.style.color = "blue";
 }
 title.addEventListener("click", handleClick);
+
+if (20 > 5 && "nico" === "neco") {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+
+const age = prompt("How old are you");
+
+if (age >= 18 && age < 21) {
+  console.log("You can drink but you should not");
+} else {
+  console.log("you cnat");
+}
