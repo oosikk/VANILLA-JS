653f8d8251da0efc5489264bdfc046ea5dea859e
leejehwan <wpghks7@naver.com>
2020-10-04 02:25:15 +0900

    2.3 Modifying the DOM with JS

--- LeeJehwan/2.PRACTICE/index.js
+++ LeeJehwan/2.PRACTICE/index.js
@@ -1,6 +1,9 @@
-const title = document.getElementById("title");
-
-console.log(title);
-console.error("err");
+// const title = document.getElementById("title");
+const title = document.querySelector("#title");
 
 title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = "I own you now";
+
+console.dir(title);
+console.dir(document);
