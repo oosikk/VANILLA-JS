7242f1e13a362b0302939567ad6329963911bcc5
leejehwan <wpghks7@naver.com>
2020-10-04 02:17:02 +0900

    2.1 More Function Fun

--- LeeJehwan/2.PRACTICE/index.js
+++ LeeJehwan/2.PRACTICE/index.js
@@ -1,5 +1,35 @@
 function sayHello(name, age) {
-  console.log("hello!", name, "you have", age, "years of age.");
+  return `hello ${name} you are ${age} years old`;
 }
 
-sayHello("Nico", 15);
+const greetNicolas = sayHello("Nico", 14);
+console.log(greetNicolas);
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  square: function (a, b) {
+    return a ** b;
+  },
+};
+
+const plus = calculator.plus(5, 5);
+console.log(plus);
+const minus = calculator.minus(5, 5);
+console.log(minus);
+const multiply = calculator.multiply(5, 5);
+console.log(multiply);
+const divide = calculator.divide(5, 5);
+console.log(divide);
+const square = calculator.square(5, 5);
+console.log(square);
